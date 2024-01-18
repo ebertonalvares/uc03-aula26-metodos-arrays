d6fd0571d9eeb9e122d1f0825c9101f180f55bd6
eberton mauricio alvares <ebertonalvares@hotmail.com>
2024-01-18 20:04:41 -0300

    Primeiro Commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// Arrays e seus métodos
+let lista = ['Banana', 'Maçã', 'Manga'];
+
+console.log(lista);
+
+// adicionar um item na ultima posição
+lista.push('Abacaxi')
+
+console.log(lista);
+
+// remover um item do final lista (indice length - 1)
+console.log('POP')
+let itemRemovido = lista.pop()
+console.log('Item Removido', itemRemovido)
+console.log('Lista Original', lista)
+
+console.log('SLICE último')
+// remove da lista o último, retornando uma nova lista sem ele
+let novaLista = lista.slice(0, -1)
+console.log('Nova Lista', novaLista)
+console.log('Lista Original', lista)
+
+// remover um item do inicio da lista (indice 0)
+console.log('SHIFT')
+itemRemovido = lista.shift()
+console.log('Item Removido', itemRemovido)
+console.log('Lista Original', lista)
+
+// remove da lista o primeiro, retornando uma nova lista sem ele
+console.log('SLICE primeiro')
+novaLista = lista.slice(1)
+console.log('Nova Lista', novaLista)
+console.log('Lista Original', lista)
+
+// alterar o valor de um item
+
+lista[0] = 'morango'
+console.log('Lista Original', lista)
+
+//Inverter valores da lista 
+lista.reverse()
+console.log('Lista Original', lista)
+
+//
